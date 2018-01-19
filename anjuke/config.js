004181c82a2d87f4e8625ef0fd750dde2f562784
546245234 <546245234@qq.com>
2018-01-19 20:10:08 +0800

    anjuke 文件

---
+++ anjuke/config.js
@@ -0,0 +1,15 @@
+module.exports={
+	//端口
+	port:8080,
+
+	//数据库
+	mysql_host:'localhost',
+	mysql_port:3306,
+	mysql_user:'root',
+	mysql_password:'123456',
+	mysql_dbname:'anjuke',
+
+	//超级管理员
+	root_username:'danbo',
+	root_password:'e10adc3949ba59abbe56e057f20f883e'
+}
